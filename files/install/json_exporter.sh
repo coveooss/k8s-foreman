b6e80c288af157c449cc22e241f720340b240451
Thomas <tdegiacinto@coveo.com>
2019-03-25 12:03:02 +0100

    remove json exporter

--- files/install/json_exporter.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-mkdir /prometheus && cd /prometheus
-git clone -b custom https://github.com/tdegiacinto/prometheus-json-exporter.git
-cd prometheus-json-exporter
-./gow get .
-./gow build -o json_exporter .
